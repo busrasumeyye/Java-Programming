130c9ba9b76ef44a121152c5ef6d931480d67d9b
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-20 13:30:12 +0300

    Lesson 5

--- intro/src/intro/Homework.java
+++ intro/src/intro/Homework.java
@@ -3,8 +3,11 @@ package intro;
 public class Homework {
 
 	public static void main(String[] args) {
+	System.out.println("Merhaba Java");
 	System.out.println("Hello World");
+	
 }
 }
 
-
+//giriş noktası main classı
+//
