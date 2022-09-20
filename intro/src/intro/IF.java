74cce8d58b2a272a904e85f0bdac7c474ae2667f
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-20 16:38:51 +0300

    if-else

--- intro/src/intro/IF.java
+++ intro/src/intro/IF.java
@@ -1,5 +1,24 @@
 package intro;
 
 public class IF {
-
+	public static void main(String[] args) {
+		//IF blokları ile çalışma
+		int sayi=20 ;
+		if(sayi<20) {
+			System.out.println("Sayı 20den küçüktür");
+		}else if(sayi==20){
+			
+			System.out.println("Sayı 20ye eşittir");
+		}else {
+			System.out.println("Sayı 20den büyüktür");
+		}
+			
+			/*else {
+		}
+			System.out.println("Sayı 20den küçük değildir");
+		}
+		/*if(sayi<15) {
+			System.out.println("Sayı 15ten küçüktür");
+		}*/
+	}
 }
