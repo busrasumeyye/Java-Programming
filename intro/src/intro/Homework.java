658c4691d231e44988c0ce09a5c32f72e2c8c3ac
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-20 13:35:45 +0300

    Lesson 5

--- intro/src/intro/Homework.java
+++ intro/src/intro/Homework.java
@@ -4,10 +4,11 @@ public class Homework {
 
 	public static void main(String[] args) {
 	System.out.println("Merhaba Java");
+	System.out.println("Merhaba Java 2");
 	System.out.println("Hello World");
 	
 }
 }
 
 //giriş noktası main classı
-//
+//intellisense
