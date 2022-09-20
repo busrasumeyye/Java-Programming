3382c3e30453abfba91d2f21f3558211086e58a2
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-20 13:53:10 +0300

    Variables

---
+++ intro/src/intro/Lesson6.java
@@ -0,0 +1,27 @@
+package intro;
+
+public class Lesson6 {
+    //variables
+	//java case sensitive bir dildir
+	//ilk kelimenin ilk harfi küçük diğer kelimelerin ilk harfi büyük
+	//camel casing
+	//reusability:tekrar tekrar kullanılabilirlik
+	public static void main(String[] args) {
+	  int ogrenciSayisi=13;
+	  String mesaj="Öğrenci sayısı ";
+	  System.out.println(mesaj + ogrenciSayisi);
+	  System.out.println("Öğrenci sayım: 9");
+	  System.out.println("Öğrenci sayım: 9");
+	  System.out.println("Öğrenci sayım: 9");
+	  System.out.println("Öğrenci sayım: "+ogrenciSayisi);
+	  System.out.println("Öğrenci sayım: 10");
+	  System.out.println(mesaj + ogrenciSayisi);
+	  System.out.println("Öğrenci sayısı: "+ogrenciSayisi);
+	  System.out.println("Öğrenci sayısı: "+ogrenciSayisi);
+	  System.out.println("Öğrenci sayısı: "+ogrenciSayisi);
+	  //+ sayesinde metinsel toplama yapılıyor
+	  
+	  
+	  
+}
+}
