b67519205024c64d39768a8e218f1f7d4ddca0ea
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-20 14:22:13 +0300

    Data Types

---
+++ intro/src/intro/DataTypes.java
@@ -0,0 +1,28 @@
+package intro;
+
+public class DataTypes {
+	public static void main(String[] args) {
+	    //java type güvenli bir dildir
+	    //değişken tanımladığımızda veri tipini bilmek ister
+	    //primitive types: bir dilin en temel veri tipleridir
+	    //long 8 byte,double 8 byte float 4 byte,double ve float ondalıklı sayı tutar
+	    int sayi=12;// 4 byte yer kaplar
+	    //sayi="Ankara"; sayi ifadesi tam sayı bir ifadedir ve ankara olarak değiştiremeyiz
+	    sayi=-130;
+	    sayi=sayi+1;;
+	    System.out.println(sayi);
+	    double say=12.5;
+	    
+	    char karakter= 'A';//metinsel bir ifade arkaplanda karakter topluluğudur
+	    boolean yanit= true;//1bytelık yer tutar
+	    boolean dogruMu= false;
+	    if(dogruMu==true) {
+	    	
+	    }
+	    
+	    
+	    
+	
+	
+}
+}
