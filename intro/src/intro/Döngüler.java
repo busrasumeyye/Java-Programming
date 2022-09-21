3aac3a26798c38b4cb9d2b5ea14d606166e51937
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-21 20:01:13 +0300

    For Döngüsü

---
+++ intro/src/intro/Döngüler.java
@@ -0,0 +1,18 @@
+package intro;
+
+public class Döngüler {
+
+	public static void main(String[] args) {
+		//for
+		int i;
+        /*for(i=1;i<=100;i++) {
+        	System.out.println(i);
+        }*/
+        for(i=1;i<10;i+=2) {
+        	System.out.println(i);
+        }
+        System.out.println("Döngü bitti");
+	
+
+}
+}
