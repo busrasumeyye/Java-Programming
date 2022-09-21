d1caa2955ab8279b3dd561ae7d9da49f9523ef49
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-21 20:07:31 +0300

    while döngüsü

---
+++ intro/src/intro/Döngüler2.java
@@ -0,0 +1,15 @@
+package intro;
+
+public class Döngüler2 {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<20) {
+			System.out.println(i);
+			i+=3;
+		}
+		System.out.println("While döngüsü bitti");
+
+	}
+
+}
