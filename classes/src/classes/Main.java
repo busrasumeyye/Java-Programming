fcf416d6f810e53d9a72813f79883f3de333ad60
Büşra Sümeyye <c1811056@student.cankaya.edu.tr>
2022-09-28 18:45:33 +0300

    class working

--- classes/src/classes/Main.java
+++ classes/src/classes/Main.java
@@ -3,6 +3,14 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager=customerManager2;
+		//bellekte customer manager türünde bir nesne ürettik
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		//classlar referens tiptir
 		
 
 	}
