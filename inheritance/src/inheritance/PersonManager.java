658f4a4edef494f95a058c9813ad5a1087a857d2
Büşra Sümeyye <c1811056@student.cankaya.edu.tr>
2022-09-28 22:05:47 +0300

    Inheritance Done

---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class PersonManager {
+	public void List() {
+		System.out.println("Listelendi");
+	}
+	
+	public void Ekle() {
+		System.out.println("Eklendi");
+	}
+
+}
