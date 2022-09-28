093a16b04f19a69ed020bb73b28ad6d1785bd86e
Büşra Sümeyye <c1811056@student.cankaya.edu.tr>
2022-09-28 21:55:54 +0300

    Method Overloading Done

--- methodOverloading/src/methodOverloading/Main.java
+++ methodOverloading/src/methodOverloading/Main.java
@@ -3,7 +3,11 @@ package methodOverloading;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DortIslem dortIslem=new DortIslem();
+		int sonuc=dortIslem.topla(5,9);
+		System.out.println(sonuc);
+		dortIslem.topla(2, 3, 5);
+		
 
 	}
 
