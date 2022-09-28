42de1c3bbd561e938544b16455908c59878d34d5
busrasumeyye <busrasumeyyetatli@gmail.com>
2022-09-28 13:24:14 +0300

    *

--- intro/src/intro/Homework.java
+++ intro/src/intro/Homework.java
@@ -1,12 +1,13 @@
 package intro;
 
 public class Homework {
-
 	public static void main(String[] args) {
+	
 	System.out.println("Merhaba Java");
 	System.out.println("Merhaba Java 2");
 	System.out.println("Hello World");
 	
+
 }
 }
 
