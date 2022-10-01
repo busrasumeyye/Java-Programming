64cc06aa0586b43078809b3dcf4bb36cdbd034af
Büşra Sümeyye <c1811056@student.cankaya.edu.tr>
2022-10-01 15:20:07 +0300

    Mini Project5 Done

--- Homeworks/src/Main.java
+++ Homeworks/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-	/*	int [] sayilar= new int[] {1,2,5,7,9,0};
+		int [] sayilar= new int[] {1,2,5,7,9,0};
 		int aranacak=6;
 		boolean varMi=false;
 		for(int sayi: sayilar) {
@@ -20,6 +20,5 @@ public class Main {
 			System.out.println("Sayi mevcut değildir");
 		}
 	}
-}*/
-	}
+
 }
