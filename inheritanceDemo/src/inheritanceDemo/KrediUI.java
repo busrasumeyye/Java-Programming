28f4beaac1037f0d28c054feb8a508b30a3e92f4
Büşra Sümeyye <c1811056@student.cankaya.edu.tr>
2022-10-02 21:21:01 +0300

    KrediUI

--- inheritanceDemo/src/inheritanceDemo/KrediUI.java
+++ inheritanceDemo/src/inheritanceDemo/KrediUI.java
@@ -2,9 +2,20 @@ package inheritanceDemo;
 
 public class KrediUI {
 	
-	public void KrediHesapla(OgretmenKrediManager ogretmenKrediManager) {
+	//public void KrediHesapla(OgretmenKrediManager ogretmenKrediManager) {
 		//
-		
+	public void KrediHesapla(BaseKrediManager baseKrediManager) {
+		baseKrediManager.Hesapla();
 	}
 
+	//referans tip olduğu için öğretmen ya da tarım gönderilebilir ve referans olarak tutulabilir
+     
+
+
+
+
+
+
+
 }
+
