f2bf3099d1edd5c8464465bbaf31a476806150da
Büşra Sümeyye <c1811056@student.cankaya.edu.tr>
2022-10-05 22:17:45 +0300

    Throw

--- oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -3,9 +3,12 @@ package oopWithNLayeredApp.business;
 import oopWithNLayeredApp.entitites.Product;
 
 public class ProductManager {
-	public void add(Product product) {//response request pattern
+	public void add(Product product) throws Exception {//response request pattern
 		//iş kuralları
-		
+		if(product.getUnitPrice()<10) {
+			throw new Exception("Ürün fiyatı 10 dan küçük olamaz");
+		}
 	}
+	
 
 }
